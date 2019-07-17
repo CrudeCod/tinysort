cc23a1e658577612063e30f5c5e53882246f2fff
shetCode <mikaelcg2@gmail.com>
2019-07-17 23:35:50 +0200

    Initial commit

---
+++ tinysort/tinysort/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace tinysort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textCheck = new System.Windows.Forms.CheckBox();
+            this.imgCheck = new System.Windows.Forms.CheckBox();
+            this.appCheck = new System.Windows.Forms.CheckBox();
+            this.vidCheck = new System.Windows.Forms.CheckBox();
+            this.sortButton = new System.Windows.Forms.Button();
+            this.directoryButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tinysort 1.0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textCheck
+            // 
+            this.textCheck.AutoSize = true;
+            this.textCheck.Location = new System.Drawing.Point(342, 32);
+            this.textCheck.Name = "textCheck";
+            this.textCheck.Size = new System.Drawing.Size(111, 21);
+            this.textCheck.TabIndex = 1;
+            this.textCheck.Text = "Sort text files";
+            this.textCheck.UseVisualStyleBackColor = true;
+            // 
+            // imgCheck
+            // 
+            this.imgCheck.AutoSize = true;
+            this.imgCheck.Location = new System.Drawing.Point(342, 97);
+            this.imgCheck.Name = "imgCheck";
+            this.imgCheck.Size = new System.Drawing.Size(105, 21);
+            this.imgCheck.TabIndex = 2;
+            this.imgCheck.Text = "Sort images";
+            this.imgCheck.UseVisualStyleBackColor = true;
+            // 
+            // appCheck
+            // 
+            this.appCheck.AutoSize = true;
+            this.appCheck.Location = new System.Drawing.Point(342, 165);
+            this.appCheck.Name = "appCheck";
+            this.appCheck.Size = new System.Drawing.Size(135, 21);
+            this.appCheck.TabIndex = 3;
+            this.appCheck.Text = "Sort applications";
+            this.appCheck.UseVisualStyleBackColor = true;
+            // 
+            // vidCheck
+            // 
+            this.vidCheck.AutoSize = true;
+            this.vidCheck.Location = new System.Drawing.Point(342, 234);
+            this.vidCheck.Name = "vidCheck";
+            this.vidCheck.Size = new System.Drawing.Size(101, 21);
+            this.vidCheck.TabIndex = 4;
+            this.vidCheck.Text = "Sort videos";
+            this.vidCheck.UseVisualStyleBackColor = true;
+            // 
+            // sortButton
+            // 
+            this.sortButton.Location = new System.Drawing.Point(130, 229);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(192, 26);
+            this.sortButton.TabIndex = 5;
+            this.sortButton.Text = "sort the directory";
+            this.sortButton.UseVisualStyleBackColor = true;
+            this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+            // 
+            // directoryButton
+            // 
+            this.directoryButton.Location = new System.Drawing.Point(33, 229);
+            this.directoryButton.Name = "directoryButton";
+            this.directoryButton.Size = new System.Drawing.Size(75, 25);
+            this.directoryButton.TabIndex = 6;
+            this.directoryButton.Text = "path";
+            this.directoryButton.UseVisualStyleBackColor = true;
+            this.directoryButton.Click += new System.EventHandler(this.directoryButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 289);
+            this.Controls.Add(this.directoryButton);
+            this.Controls.Add(this.sortButton);
+            this.Controls.Add(this.vidCheck);
+            this.Controls.Add(this.appCheck);
+            this.Controls.Add(this.imgCheck);
+            this.Controls.Add(this.textCheck);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox textCheck;
+        private System.Windows.Forms.CheckBox imgCheck;
+        private System.Windows.Forms.CheckBox appCheck;
+        private System.Windows.Forms.CheckBox vidCheck;
+        private System.Windows.Forms.Button sortButton;
+        private System.Windows.Forms.Button directoryButton;
+    }
+}
+
---
+++ tinysort/tinysort/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tinysort
+{
+    public partial class Form1 : Form
+    {
+        //initializes the directory path and the target path
+        string dirPath;
+        string targPath;
+        //initializes the possible extensions
+        string[] textExtensions = { ".txt", ".rtf", ".pdf", ".doc", ".docx" };
+        string[] imageExtensions = { ".png", ".jpg", ".jpeg", ".gif" };
+        string[] videoExtensions = { ".mp4", ".m4a", ".mov", ".ogg", ".webm", ".wmv", ".avi", ".flv" };
+        string[] appExtensions = { ".exe",".url" };
+
+
+        System.Collections.ArrayList fileNames = new System.Collections.ArrayList();
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            //activates the sorting algorithm
+            sortMethod(); 
+
+        }
+
+        private void sortMethod() {
+            // collects names of files in a given directory and lumps them in an array
+            string[] sFileNames = Directory.GetFiles(dirPath, "*");
+
+            foreach (string element in sFileNames)
+            {
+                //converts the full paths to the files to just their actual names and adds them into an ArrayList
+                fileNames.Add(Path.GetFileName(element)); 
+            }
+            //checks if directory doesn't already exist, then creates it
+            if (!Directory.Exists(Path.Combine(dirPath, "images"))) {
+                Directory.CreateDirectory(Path.Combine(dirPath, "images"));
+            }
+            if (!Directory.Exists(Path.Combine(dirPath, "text_files"))) {
+                Directory.CreateDirectory(Path.Combine(dirPath, "text_files"));
+            }
+
+            if (!Directory.Exists(Path.Combine(dirPath, "videos"))) {
+                Directory.CreateDirectory(Path.Combine(dirPath, "videos"));
+            }
+
+            if (!Directory.Exists(Path.Combine(dirPath, "applications"))) {
+                Directory.CreateDirectory(Path.Combine(dirPath, "applications"));
+            }
+                
+
+            
+            foreach (string fileName in fileNames) {
+                //extension check
+
+                if (imageExtensions.Contains(Path.GetExtension(fileName))) {
+                    if (imgCheck.Checked)
+                    {
+                        //assigns the target path
+                        targPath = Path.Combine(dirPath, "images");
+
+                        //creates the source file location
+                        string sourceFile = Path.Combine(dirPath, fileName);//implement this as showed on that blogspot website
+                        //creates the destination
+                        string destFile = Path.Combine(targPath, fileName);
+                        //initiates the transfer + checks for unknown exceptions
+                        try
+                        {
+                            File.Move(sourceFile, destFile);
+                        }
+                        catch (Exception e)
+                        {
+                            System.Windows.Forms.MessageBox.Show("An exception has occured! " + e);
+
+                        }
+
+
+                    }
+                }
+
+
+                //    if (Path.GetExtension(fileName) == ".png" || Path.GetExtension(fileName) == ".jpg" || Path.GetExtension(fileName) == ".jpeg" || Path.GetExtension(fileName) == ".gif") {
+
+                else if (textExtensions.Contains(Path.GetExtension(fileName)))
+                {
+                    if (textCheck.Checked) {
+
+                        //assigns the target path
+                        targPath = Path.Combine(dirPath, "text_files");
+                        //creates the source file location
+                        string sourceFile = Path.Combine(dirPath, fileName);//implement this as showed on that blogspot website
+                        //creates the destination
+                        string destFile = Path.Combine(targPath, fileName);
+                        //initiates the transfer
+                        try
+                        {
+                            File.Move(sourceFile, destFile);
+                        }
+                        catch (Exception e)
+                        {
+                            //checks if a fuck-up happens
+                            System.Windows.Forms.MessageBox.Show("An exception has occured! " + e);
+
+                        }
+
+                    }
+
+                }
+                else if (videoExtensions.Contains(Path.GetExtension(fileName)))
+                {
+
+                    if (vidCheck.Checked) {
+                        targPath = Path.Combine(dirPath, "videos");
+
+                        string sourceFile = Path.Combine(dirPath, fileName);
+
+                        string destFile = Path.Combine(targPath, fileName);
+
+                        try
+                        {
+                            File.Move(sourceFile, destFile);
+                        }
+                        catch (Exception e)
+                        {
+                            System.Windows.Forms.MessageBox.Show("An exception has occured! " + e);
+
+                        }
+
+                    }
+
+                }
+                else if (appExtensions.Contains(Path.GetExtension(fileName)))
+                {
+                    if (appCheck.Checked) {
+                        targPath = Path.Combine(dirPath, "applications");
+
+                        string sourceFile = Path.Combine(dirPath, fileName);
+
+                        string destFile = Path.Combine(targPath, fileName);
+
+                        try
+                        {
+                            File.Move(sourceFile, destFile);
+                        }
+                        catch (Exception e)
+                        {
+                            System.Windows.Forms.MessageBox.Show("An exception has occured! " + e);
+
+                        }
+
+
+                    }
+
+                }
+              
+            }
+        }
+
+        
+
+        private void directoryButton_Click(object sender, EventArgs e)
+        {
+            //makes a folder browse dialog
+            FolderBrowserDialog openDir = new FolderBrowserDialog();
+            //sets the dirPath variable to the directory choice of the user
+            if (openDir.ShowDialog() == DialogResult.OK) {
+                 dirPath = openDir.SelectedPath;
+     
+
+            }
+        }
+    }
+}
